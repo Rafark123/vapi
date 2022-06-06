839d6ebaa23def00ff8890b0c80f9ea67bffd7ac
Rafark123 <zumbidoalasca@gmail.com>
2022-06-06 17:43:43 -0300

    a

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require(`express`)
 const app = express()
-const port = 8080 || process.env.PORT
+const port = process.env.PORT
 
 app.use(express.json({limit: "50mb"}));
 app.use(express.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
